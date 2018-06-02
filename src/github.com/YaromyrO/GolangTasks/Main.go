a2aefd657409a4a5b581688f3996b1fab047d860
Yaromyr <oryshchyn97@gmail.com>
2018-06-02 16:25:36 +0300

    -

--- src/github.com/YaromyrO/GolangTasks/Main.go
+++ src/github.com/YaromyrO/GolangTasks/Main.go
@@ -1,19 +1,6 @@
 package main
 
-import (
-	"./sorting/counting_sort"
-	"fmt"
-	"math/rand"
-	"time"
-)
-
 func main() {
 
-	rand.Seed(time.Now().Unix())
-	slice := make([]int, 50)
-	for i := 0; i < len(slice); i++ {
-		slice[i] = rand.Intn(100 - 1) + 1
-	}
-	fmt.Println(slice)
-	fmt.Println(counting_sort.CountingSort(slice))
+
 }
