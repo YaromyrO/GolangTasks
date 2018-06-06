170c41a7c53fae7cfd9bbec116d7bec503ca945d
Yaromyr <oryshchyn97@gmail.com>
2018-06-06 15:16:16 +0300

    changes

--- src/github.com/YaromyrO/GolangTasks/Main.go
+++ src/github.com/YaromyrO/GolangTasks/Main.go
@@ -12,16 +12,20 @@ func main() {
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
 
-	slice := make([]int, 1000)
+	slice := make([]int, 10000)
 	for i := range slice {
-		slice[i] = r1.Intn(200)
+		slice[i] = r1.Intn(1000)
 	}
 	//slice := []int{10, 15, 7, 5, 13, 22, 6, 9}
 	fmt.Println(slice)
 	//quicksort.QuickSort(slice, quicksort.GetPivot)
-	//fmt.Println(slice)
+	//fmt.Println
+
 	//fmt.Println(counting_sort.CountingSort(slice))
 	//fmt.Println(merge_sort.MergeSort(slice))
+	before := time.Now()
 	fmt.Println(bucket_sort.BucketSort(slice, bucket_sort.MsBits))
+	after := time.Now()
+	println(after.Sub(before) /1000000)
 
 }
--- src/github.com/YaromyrO/GolangTasks/sorting/bucket_sort/bucketSort.go
+++ src/github.com/YaromyrO/GolangTasks/sorting/bucket_sort/bucketSort.go
@@ -1,14 +1,17 @@
 package bucket_sort
 
+import "sort"
+
 func MsBits(input []int, item int) int {
 
-	min := input[0]
 	max := input[0]
-	for i := 1; i < len(input); i++ {
-		if input[i] < min {
-			min = input[i]
-		} else if input[i] > max {
-			max = input[i]
+	min := input[0]
+	for _, value := range input {
+		if max < value {
+			max = value
+		}
+		if min > value {
+			min = value
 		}
 	}
 	return ((item - min) * len(input)) / (max - min + 1)
@@ -20,7 +23,7 @@ func BucketSort(input []int, getIndex func([]int, int) int) []int {
 		return input
 	}
 
-	bucket := make([][]int, len(input))
+	bucket := make(map[int][]int)
 
 	for _, v := range input {
 		if len(bucket[getIndex(input, v)]) == 0 {
@@ -30,10 +33,13 @@ func BucketSort(input []int, getIndex func([]int, int) int) []int {
 	}
 
 	for _, v := range input {
-		if len(bucket[getIndex(input, v)]) < 1 && len(bucket[getIndex(input, v)]) != 0 {
-			bucket[getIndex(input, input[v])] = BucketSort(bucket[getIndex(input, input[v])], MsBits)
+		if len(bucket[getIndex(input, v)]) > 1 {
+			//bucket[getIndex(input, v)] = insertionSort(bucket[getIndex(input, v)])
+			sort.Ints(bucket[getIndex(input, v)])
 		}
 	}
+
+
 
 	x := 0
 	for i := 0; i < len(input); i++ {
@@ -41,7 +47,20 @@ func BucketSort(input []int, getIndex func([]int, int) int) []int {
 			input[x] = v
 			x++
 		}
-
 	}
 	return input
+}
+
+func insertionSort(slice []int) []int {
+
+	for i := 1; i < len(slice); i++ {
+		temp := slice[i]
+		j := i - 1
+		for j >= 0 && slice[j] > temp {
+			slice[j+1] = slice[j]
+			j = j - 1
+		}
+		slice[j+1] = temp
+	}
+	return slice
 }
